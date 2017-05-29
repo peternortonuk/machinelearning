228ce97847ca499982c0d70b972da9c6ee596e78
Peter Norton <pnorton@gazprom-mt.com>
2017-05-29 21:36:02 +0100

    start on regression

--- scikit_tutorial/statistical_inference.py
+++ scikit_tutorial/statistical_inference.py
@@ -1,7 +1,13 @@
+# ==========================================================================
+# http://scikit-learn.org/stable/tutorial/statistical_inference/supervised_learning.html
 
+
+# ==========================================================================
+# Nearest neighbor and the curse of dimensionality
 
 import numpy as np
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier
 
 iris = datasets.load_iris()
 iris_X = iris.data
@@ -18,13 +24,42 @@ iris_y_train = iris_y[indices[:-10]]
 iris_X_test  = iris_X[indices[-10:]]
 iris_y_test  = iris_y[indices[-10:]]
 
-# Create and fit a nearest-neighbor classifier
-from sklearn.neighbors import KNeighborsClassifier
+# create and fit a nearest-neighbor classifier
 knn = KNeighborsClassifier()
 knn.fit(iris_X_train, iris_y_train)
 
+
+print(knn.predict(iris_X_test))
+
+print(iris_y_test)
+
 print('break')
 
-knn.predict(iris_X_test)
 
-iris_y_test
+# ==========================================================================
+# Linear model: from regression to sparsity
+
+from sklearn import linear_model
+
+diabetes = datasets.load_diabetes()
+diabetes_X_train = diabetes.data[:-20]
+diabetes_X_test  = diabetes.data[-20:]
+diabetes_y_train = diabetes.target[:-20]
+diabetes_y_test  = diabetes.target[-20:]
+
+regr = linear_model.LinearRegression()
+regr.fit(diabetes_X_train, diabetes_y_train)
+print(regr.coef_)
+
+# The mean square error
+mse = np.mean((regr.predict(diabetes_X_test)-diabetes_y_test)**2)
+print(mse)
+
+# Explained variance score: 1 is perfect prediction
+# and 0 means that there is no linear relationship
+# between X and y.
+score = regr.score(diabetes_X_test, diabetes_y_test)
+print(score)
+
+
+print('break')
