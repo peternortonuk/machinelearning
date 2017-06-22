ad97b7fc373015632b0e1c9a0a8634da4712a55f
Peter Norton <peter.norton.uk@gmail.com>
2017-06-22 22:51:43 +0100

    use train test helper fn2

--- sorbus/sorbus.py
+++ sorbus/sorbus.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import os
 from sklearn import neighbors
+from sklearn.model_selection import train_test_split
 from utilities import plot_knn
 
 pathname = r'C:\dev\code\machinelearning\data'
@@ -20,58 +21,54 @@ for sheetname in sheetnames:
     df = pd.read_excel(pathfile, sheetname=sheetname, )
     list_of_df.append(df)
 df_all = pd.concat(list_of_df, axis=0, join='inner', copy=True)
+df_all.reset_index(drop=True, inplace=True)
 
-# calculate the train and test row count and slicer
+# calculate the minimum row count to ensure equal group sizes
 df_count = df_all.groupby(by='Species').count().iloc[:,0]
-total_row_count = df_count.min()
-train_row_count = int(total_row_count*train_factor)
-test_row_count = total_row_count - train_row_count
+group_row_count = df_count.min()
 
-# add a column with the usage type and label as test versus train
+# create a new df with equal sized groups
 unique_species = df_all['Species'].unique()
 np.ndarray.sort(unique_species)
+df_equal = pd.DataFrame()
 for i, species in enumerate(unique_species):
     species_mask = df_all['Species'] == species
     # add integer code for use in charting later
     df_all.loc[species_mask,'Species_code'] = i
     # monumental faff due to assignment fail when using chained indexing
     # http://pandas.pydata.org/pandas-docs/stable/indexing.html#indexing-view-versus-copy
-    # train
-    ix = df_all.index[species_mask][:train_row_count]
-    df_all.loc[ix, 'Usage'] = 'train'
-    # test
-    ix = df_all.index[species_mask][train_row_count: train_row_count+test_row_count]
-    df_all.loc[ix, 'Usage'] = 'test'
+    ix = df_all.index[species_mask][:group_row_count]
+    df_subset = df_all.loc[ix, :]
+    df_equal = pd.concat([df_equal, df_subset], axis=0)
 
-
-# create train and test data for the model
-train_mask = df_all['Usage'] == 'train'
-test_mask = df_all['Usage'] == 'test'
-model_mask = df_all['Usage'].isin(['train', 'test'])
+# create arrays of required data
 X_columns = ['leaf length', 'leaf width', 'widest point', 'total veins']
 y_columns = ['Species']
-X = df_all[train_mask][X_columns].values
-y = df_all[train_mask][y_columns].values
-Z = df_all[test_mask][X_columns].values
-ZT = df_all[test_mask][y_columns].values
+X = df_equal[X_columns].values
+y = df_equal[y_columns].values
+
+# use helper function to split into train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=train_factor, random_state=0)
 
 # fit the training data
-n_neighbors = 15
+n_neighbors = 10
 weights = ['uniform', 'distance']
 clf = neighbors.KNeighborsClassifier(n_neighbors, weights=weights[0])
-clf.fit(X, y.ravel())
+clf.fit(X_train, y_train)
 
 # predict the test data
-output = clf.predict(Z)
-result = output == ZT.ravel()
+output = clf.predict(X_test)
+result = output == y_test
 print np.unique(result, return_counts=True)
 
 # charting with 2D only; use all the model data
 # also requires numeric chart labels (instead of species name)
+# create new arrays of required data
 X_columns = ['leaf length', 'leaf width']
 y_columns = ['Species_code']
-X = df_all[model_mask][X_columns].values
-y = df_all[model_mask][y_columns].values
-plot_knn(X, y, n_neighbors)
+X = df_equal[X_columns].values
+y = df_equal[y_columns].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=train_factor, random_state=0)
+plot_knn(X_test, y_test, n_neighbors)
 
 print('cheese')
