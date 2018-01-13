4feb832b7730b3803bfb1a5aee360b55258612ca
Peter Norton <peter.norton.uk@gmail.com>
2018-01-13 16:43:05 +0000

    reorg files and tidy

--- coursera/neural_networks/assignment01.py
+++ coursera/neural_networks/assignment01.py
@@ -1,15 +1,28 @@
-import utils
-import assignment_utils
+import assignment01_utils
 import matplotlib.pyplot as plt
 
+# image file definitions
 path = r'C:\dev\code\machinelearning\coursera\neural_networks\datasets'
 file_train = r'train_catvnoncat.h5'
 file_test = r'test_catvnoncat.h5'
 
-train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = assignment_utils.load_data(path, file_train, file_test)
-train_set_x, test_set_x = assignment_utils.prepare_data(train_set_x_orig, test_set_x_orig)
-d = utils.model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+# load data
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = assignment01_utils.load_data(path,
+                                                                                                   file_train,
+                                                                                                   file_test)
 
+# reshape the image data into feature vectors
+train_set_x, test_set_x = assignment01_utils.prepare_data(train_set_x_orig,
+                                                          test_set_x_orig)
+
+# run the model
+d = assignment01_utils.model(train_set_x,
+                             train_set_y,
+                             test_set_x,
+                             test_set_y,
+                             num_iterations = 2000,
+                             learning_rate = 0.005,
+                             print_cost = True)
 
 # extract the results
 activation = d["Activation_test"][0]
@@ -20,17 +33,20 @@ sorted_index = activation.argsort()
 
 # sort the data
 activation = activation[sorted_index]
-test_set_x = test_set_x[:, sorted_index]
+test_set_x_orig = test_set_x_orig[sorted_index]
 y_prediction = y_prediction[:,sorted_index]
 
 # plot images
-num_px = test_set_x_orig.shape[1]
-for index in range(test_set_x.shape[1]):
+for index in range(len(test_set_x_orig)):
+    # plot the image
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+    plt.imshow(test_set_x_orig[index])
+
+    # annotate
     text_Y = classes[y_prediction[0,index].astype(int)].decode("utf-8")
-    text_A = str(activation[index]*100)
-    text = text_Y + " at " + text_A[:5] + " percent"
-    ax.annotate(text, fontsize=20, xy=(2, 1), xytext=(3, -1.5))
+    text_A = str(activation[index])
+    text = text_Y + ": cat at P(" + text_A[:5] + ")"
+    ax.annotate(text, fontsize=20, xy=(2, 1), xytext=(0, -1.5))
+
     plt.show()
