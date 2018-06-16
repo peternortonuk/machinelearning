081857b83c0e12bb4f129b2164ad30007f0aa96f
pnorton <peter.norton@gazprom-mt.com>
2018-06-16 22:52:25 +0100

    first stab

---
+++ metadata/analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+from  itertools import combinations
+
+file = r'arc_metadata.csv'
+path = r'C:\dev\code\machinelearning\data'
+filepath = os.path.join(path, file)
+df = pd.read_csv(filepath)
+
+mask_data_source = df['DataSource'] == 'APX'
+df_select = df[mask_data_source]
+
+rowcount = len(df_select.index)
+column_count = len(df_select.columns)
+combo_select = 2
+
+# a list of all combinations
+groupby_columns = list(combinations(df_select.columns, combo_select))
+
+# every combination
+df_analysis = pd.DataFrame()
+for groupby_column in groupby_columns:
+    # count distinct
+    df_counts = df_select.groupby(groupby_column).nunique()
+
+    # include the combo definition as a new column
+    # won't generate a groupby where one element is NaN; so count again
+    df_counts['groupby_column'] = [groupby_column] * len(df_counts.index)
+
+    df_analysis = pd.concat([df_analysis, df_counts], axis=0)
+
+import pdb; pdb.set_trace()
+
+pass
