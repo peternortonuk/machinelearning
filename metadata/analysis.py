f7e6ee26f4f12adc8303a4978b77e97a96b0a5f6
pnorton <peter.norton@gazprom-mt.com>
2018-06-29 16:59:13 +0100

    metadata wip

--- metadata/analysis.py
+++ metadata/analysis.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import os
 from itertools import combinations
-#import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt
+#import mplcursors
 
 file = r'arc_metadata.csv'
 path = r'C:\dev\code\machinelearning\data'
@@ -16,44 +16,66 @@ rowcount = len(df_select.index)
 column_count = len(df_select.columns)
 combo_select = 2
 
+columns_select = [#'DataSource',
+                  'Commodity',
+                  'ProductOrMarket',
+                  'Location',
+                  'ContractType',
+                  'PriceType',
+                  #'Granularity',
+                  #'PublicationFrequency',
+                  #'PublicationCalendar',
+                  'Currency',
+                  'CurveUnit',
+                  'CurveType',
+                  'UsageType',
+                  'OwnerRole',
+                  'Destination',
+                  'CurveStatus',
+                  'TimeOfDay',
+                  'Timezone',
+                  ]
+columns_select = columns_select[:5]
+
 # a list of all combinations
-groupby_columns = list(combinations(df_select.columns, combo_select))
-groupby_columns = [('ContractType', 'TimeOfDay')]
+groupby_columns = list(combinations(columns_select, combo_select))
+# groupby_columns = [('ContractType', 'TimeOfDay')] + groupby_columns[:2]
 
 # every combination
-df_analysis = pd.DataFrame()
+dict_of_df = {}
 for groupby_column in groupby_columns:
     # count distinct
     df_counts = df_select.groupby(groupby_column).nunique()
 
-    # include the combo definition as a new column; populate with a list of tuples
-    # note that we wont get a groupby where one element is NaN; so count again
-    count_of_groups = len(df_counts.index)
-    df_counts['groupby_column'] = [groupby_column] * count_of_groups
-
     # count includes the groupby as an index; we want to combine to a single index
     a0 = df_counts.index.get_level_values(0).values.tolist()
     a1 = df_counts.index.get_level_values(1).values.tolist()
-    import pdb; pdb.set_trace()
     l = list(zip(a0, a1))
-    l = (' | '.join(w) for w in l)
+    l = [' || '.join((str(i0), str(i1))) for i0, i1 in l]
     index = pd.Index(l)
     df_counts['Index'] = index
     df_counts.set_index('Index', drop=True, inplace=True)
 
     # build up the list of results
-    df_analysis = pd.concat([df_analysis, df_counts], axis=0)
+    dict_of_df[groupby_column] = df_counts
 
+number_of_subplots = len(groupby_columns)
+fig, axes = plt.subplots(number_of_subplots, 1,
+                         figsize=(6, number_of_subplots * 6))
+for i, key in enumerate(dict_of_df.keys()):
+    print(key)
+    try:
+        dict_of_df[key].transpose().plot(ax=axes[i], kind='bar', legend=False)
+        axes[i].set_title(str(key))
+        plt.subplots_adjust(top=0.1,
+                            bottom=0.1,
+                            left=0.1,
+                            right=0.1,
+                            hspace=1.0,
+                            wspace=0.5)
 
-
-
-#pd.DataFrame.plot(x='groupby_column', y=)
-
-# Note that using plt.subplots below is equivalent to using
-# fig = plt.figure() and then ax = fig.add_subplot(111)
-# fig, ax = plt.subplots()
-#ax.plot(t, s)
-
-
+    except:
+        continue
+plt.show()
 
 pass
